03f48f5e52455b3fde729c1a70fa2ee1a8206e73
ElijahRosal <elijah,rosal04@gmail.com>
2026-03-05 13:39:25 -0800

    add chapter 11 changes

--- craftingInterpreters/src/com/craftinginterpreters/lox/Interpreter.java
+++ craftingInterpreters/src/com/craftinginterpreters/lox/Interpreter.java
@@ -1,5 +1,7 @@
 package com.craftinginterpreters.lox;
 import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
 import java.util.List;
 class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
     private static class BreakSignal extends RuntimeException {
@@ -9,6 +11,7 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
     }
     final Environment globals = new Environment();
     private Environment environment = globals;
+    private final Map<Expr, Integer> locals = new HashMap<>();
     Interpreter() {
         globals.define("clock", new LoxCallable() {
             @Override
@@ -51,7 +54,15 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
     }
     @Override
     public Object visitVariableExpr(Expr.Variable expr) {
-        return environment.get(expr.name);
+        return lookUpVariable(expr.name, expr);
+    }
+    private Object lookUpVariable(Token name, Expr expr) {
+        Integer distance = locals.get(expr);
+        if (distance != null) {
+            return environment.getAt(distance, name.lexeme);
+        } else {
+            return globals.get(name);
+        }
     }
     private void checkNumberOperand(Token operator, Object
             operand) {
@@ -100,6 +111,9 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
     }
     private void execute(Stmt stmt) {
         stmt.accept(this);
+    }
+    void resolve(Expr expr, int depth) {
+        locals.put(expr, depth);
     }
     void executeBlock(List<Stmt> statements,
                       Environment environment) {
@@ -180,7 +194,12 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
     @Override
     public Object visitAssignExpr(Expr.Assign expr) {
         Object value = evaluate(expr.value);
-        environment.assign(expr.name, value);
+        Integer distance = locals.get(expr);
+        if (distance != null) {
+            environment.assignAt(distance, expr.name, value);
+        } else {
+            globals.assign(expr.name, value);
+        }
         return value;
     }
     @Override
