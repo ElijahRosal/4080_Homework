4b69333b74cc84b3a909d75d7b3e0aff6f7ff8ac
ElijahRosal <elijah,rosal04@gmail.com>
2026-03-26 18:31:44 -0700

    add: Chapter 17 Question 3

---
+++ Homework_8/Chapter_17/Question_3/scanner.h
@@ -0,0 +1,41 @@
+// Elijah Rosal - CS4080 - Homework 8, Chapter 17 Question 3
+// 3.26.2026
+/*
+Code below has been modified for Question 3 for Chapter 17 of Crafting Interpreters.
+
+Adds scanner token definitions for the ternary conditional operator (?:)
+so the parser can recognize question-mark and colon tokens.
+*/
+
+#ifndef clox_scanner_h
+#define clox_scanner_h
+typedef enum {
+    // Single-character tokens.
+    TOKEN_LEFT_PAREN, TOKEN_RIGHT_PAREN,
+    TOKEN_LEFT_BRACE, TOKEN_RIGHT_BRACE,
+    TOKEN_COMMA, TOKEN_DOT, TOKEN_QUESTION, TOKEN_COLON,
+    TOKEN_MINUS, TOKEN_PLUS,
+    TOKEN_SEMICOLON, TOKEN_SLASH, TOKEN_STAR,
+    // One or two character tokens.
+    TOKEN_BANG, TOKEN_BANG_EQUAL,
+    TOKEN_EQUAL, TOKEN_EQUAL_EQUAL,
+    TOKEN_GREATER, TOKEN_GREATER_EQUAL,
+    TOKEN_LESS, TOKEN_LESS_EQUAL,
+    // Literals.
+    TOKEN_IDENTIFIER, TOKEN_STRING, TOKEN_NUMBER,
+    // Keywords.
+    TOKEN_AND, TOKEN_CLASS, TOKEN_ELSE, TOKEN_FALSE,
+    TOKEN_FOR, TOKEN_FUN, TOKEN_IF, TOKEN_NIL, TOKEN_OR,
+    TOKEN_PRINT, TOKEN_RETURN, TOKEN_SUPER, TOKEN_THIS,
+    TOKEN_TRUE, TOKEN_VAR, TOKEN_WHILE,
+    TOKEN_ERROR, TOKEN_EOF
+   } TokenType;
+typedef struct {
+    TokenType type;
+    const char* start;
+    int length;
+    int line;
+} Token;
+void initScanner(const char* source);
+Token scanToken();
+#endif
